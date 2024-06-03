4424fc6d3c1a1f0f35a8388b067f063244593a54
yeeeun <kmjnh34@gmail.com>
2024-06-03 17:28:23 +0900

    h

--- resources/js/firebase.js
+++ resources/js/firebase.js
@@ -6,6 +6,10 @@ import {
   setDoc,
   doc,
   addDoc,
+  //여기에 쓰는건 firebase.js 에서 필요한것들..
+  deleteDoc,
+  updateDoc,
+  getDoc,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -43,4 +47,23 @@ async function addDatas(collectionName, dataObj) {
     return false;
   }
 }
-export { db, getDatas, addDatas };
+
+async function deleteDatas(collectionName, docId) {
+  const docRef = await doc(db, collectionName, docId);
+  await deleteDoc(docRef);
+  // deleteDoc("삭제할 문서 객체")
+}
+
+//이게 결국에 firebase 에 내용을 가져오고 수정하고 저장하고 삭제하고 이런것들을
+//하기위해 이쪽에 함수를 만드는거야.
+async function updateDatas(collectionName, docId, updateinfoObj) {
+  // doc(db, collectionName, 문서ID);
+  // getDoc(문서레퍼런스 )
+  // updateDoc(문서데이터, 수정할 정보객체 )
+
+  const docRef = await doc(db, collectionName, docId);
+  // const docData = await getDoc(docRef);
+  await updateDoc(docRef, updateinfoObj);
+}
+export { db, getDatas, addDatas, deleteDatas, updateDatas };
+//export 는 말그대로 내보내서 쓰는것들이니까 deleteDats를 쓴거야.
