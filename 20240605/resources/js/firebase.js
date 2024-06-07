766be7667de4437038997ca63be2462f8386ed04
yeeeun <kmjnh34@gmail.com>
2024-06-07 17:04:10 +0900

    fadsf

---
+++ 20240605/resources/js/firebase.js
@@ -0,0 +1,69 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  setDoc,
+  doc,
+  addDoc,
+  //여기에 쓰는건 firebase.js 에서 필요한것들..
+  deleteDoc,
+  updateDoc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMjH2u7MdHMfoe0MxxjYsMeBPKq0tW1Zg",
+  authDomain: "mbti-project-50b8c.firebaseapp.com",
+  projectId: "mbti-project-50b8c",
+  storageBucket: "mbti-project-50b8c.appspot.com",
+  messagingSenderId: "575450297390",
+  appId: "1:575450297390:web:71fdabff39be0960fe5bbf",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function getDatas(collectionName) {
+  const collect = await collection(db, collectionName);
+  const snapshot = await getDocs(collect);
+  return snapshot;
+}
+
+async function addDatas(collectionName, dataObj) {
+  try {
+    // 문서 ID 수동
+    // const saveDoc = await doc(db, collectionName, "2");
+    // console.log(`Doc(결과)${saveDoc}`);
+    // const saveResult = await setDoc(saveDoc, dataObj);
+    // setDoc 은 return 이 없어서 처리를 해주어야 한다.
+    // console.log(`setDoc(결과)${saveResult}`);
+    // 문서 ID 자동
+    const collect = await collection(db, collectionName);
+    await addDoc(collect, dataObj);
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+async function deleteDatas(collectionName, docId) {
+  const docRef = await doc(db, collectionName, docId);
+  await deleteDoc(docRef);
+  // deleteDoc("삭제할 문서 객체")
+}
+
+//이게 결국에 firebase 에 내용을 가져오고 수정하고 저장하고 삭제하고 이런것들을
+//하기위해 이쪽에 함수를 만드는거야.
+async function updateDatas(collectionName, docId, updateinfoObj) {
+  // doc(db, collectionName, 문서ID);
+  // getDoc(문서레퍼런스 )
+  // updateDoc(문서데이터, 수정할 정보객체 )
+
+  const docRef = await doc(db, collectionName, docId);
+  // const docData = await getDoc(docRef);
+  await updateDoc(docRef, updateinfoObj);
+}
+export { db, getDatas, addDatas, deleteDatas, updateDatas };
+//export 는 말그대로 내보내서 쓰는것들이니까 deleteDats를 쓴거야.
