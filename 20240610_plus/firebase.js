6a1071e6d50047eeb36b56e4df8ac8e88cd6c073
yeeeun <kmjnh34@gmail.com>
2024-06-10 20:48:21 +0900

    20240610

---
+++ 20240610_plus/firebase.js
@@ -0,0 +1,28 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_Pxxjr-V2b5dXe6tdoufjb7B6zwBtEbU",
+  authDomain: "myproject-dbbec.firebaseapp.com",
+  projectId: "myproject-dbbec",
+  storageBucket: "myproject-dbbec.appspot.com",
+  messagingSenderId: "547072391356",
+  appId: "1:547072391356:web:33f72108eaa649ecb5f8b9",
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+async function getDatas(collectionName) {
+  const querySnapshot = await db.collection(collectionName).get();
+  return querySnapshot;
+}
+async function addDatas(collectionName, addObj) {
+  const result = await db.collection(collectionName).add(addObj);
+  return result;
+}
+async function deleteDatas(collectionName, docId) {
+  try {
+    await db.collection(collectionName).doc(docId).delete();
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
