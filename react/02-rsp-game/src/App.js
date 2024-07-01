feed9d4855e2a61fd9f327fc80e08f05e4b94bc5
KMK23 <kmjnh34@gmail.com>
2024-07-01 17:55:02 +0900

    ㅎ

--- react/02-rsp-game/src/App.js
+++ react/02-rsp-game/src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
-
+import reset from "./assets/ic-reset.svg";
+import myImg from "./assets/paper.svg";
+import otherImg from "./assets/rock.svg";
 function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="App-titleBox">
+        <h1>가위바위보</h1>
+        <img src={reset}></img>
+      </div>
+      <div className="pointBox">
+        <div className="myPoint box">
+          <h1 className="point">5</h1>
+          <p>나</p>
+        </div>
+        <div className="qt">:</div>
+        <div className="otherPoint box">
+          <h1 className="point">1</h1>
+          <p>상대</p>
+        </div>
+      </div>
     </div>
   );
 }
