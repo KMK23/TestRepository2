429b051b7239318300a4ec9fed3171c44ad53214
KMK23 <kmjnh34@gmail.com>
2024-07-02 11:48:59 +0900

    h

--- react/02-rsp-game/src/App.js
+++ react/02-rsp-game/src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import reset from "./assets/ic-reset.svg";
-
 import { useState } from "react";
-import paper from "./assets/paper.svg";
-import rock from "./assets/rock.svg";
-import scissor from "./assets/scissor.svg";
-import Rsp from "./Rsp.js";
+import HandIcon from "./HandIcon.js";
+import "./HandIcon.css";
+import HandButton from "./HandButton.js";
 
 // 우선 내가 가위바위보중에 하나를 눌러
 // 그다음 상대는 랜덤하게 나와
@@ -13,11 +11,12 @@ import Rsp from "./Rsp.js";
 // 승 패 무를 따져
 // 그걸 승부기록에 입력하고
 // 그다음 점수는 누적으로 더해져
-const choice = [
-  { id: 1, name: "rock", img: rock },
-  { id: 2, name: "scissor", img: scissor },
-  { id: 3, name: "paper", img: paper },
-];
+
+// const choice = [
+//   { id: 1, name: "rock", img: rock },
+//   { id: 2, name: "scissor", img: scissor },
+//   { id: 3, name: "paper", img: paper },
+// ];
 // console.log(choice);
 
 function getRandomChoice(choice) {
@@ -57,26 +56,47 @@ function App() {
   };
   return (
     <div className="App">
-      <div className="App-titleBox">
-        <h1>가위바위보</h1>
-        <img src={reset}></img>
+      <h1 className="App-heading">가위바위보</h1>
+      <img src={reset} className="App-reset" />
+      <div className="App-scores">
+        <div className="Score">
+          <div className="Score-num">0</div>
+          <div className="Score-name">나</div>
+        </div>
+        <div className="App-versus">:</div>
+        <div className="Score">
+          <div className="Score-num">1</div>
+          <div className="Score-name">너</div>
+        </div>
       </div>
-      <Rsp
-        myChoice={myChoice}
-        otherChoice={otherChoice}
-        score={score}
-        result={result}
-      />
-      <div className="rspBox">
-        {choice.map((choice) => (
-          <div
-            key={choice.id}
-            className={choice.name}
-            onClick={() => handleClick(choice)}
-          >
-            <img src={choice.img} className="Img" alt={choice.name} />
+      <div className="Box App-box">
+        {/* 가위바위보 내는곳 */}
+        <div className="App-hands">
+          <div className="Hand">
+            <HandIcon className="Hand-icon" value="rock" />
           </div>
-        ))}
+          <div className="App-versus">VS</div>
+          <div className="Hand">
+            <HandIcon className="Hand-icon" value="rock" />
+            {/* 여기에다 쓴 className 은 우리가 아는 className이 아니고 props 이야 */}
+          </div>
+        </div>
+        {/* 배점 */}
+        <div className="App-bet">
+          <span>배점</span>
+          <input type="number" min={1} max={9} />
+          <span>배</span>
+        </div>
+        {/* 기록 */}
+        <div className="App-history">
+          <h2>승부기록</h2>
+          <p>승리, 패배, 무승부</p>
+        </div>
+      </div>
+      <div>
+        <HandButton value="rock" />
+        <HandButton value="scissor" />
+        <HandButton value="paper" />
       </div>
     </div>
   );
---
+++ react/02-rsp-game/src/HandButton.js
@@ -0,0 +1,9 @@
+import HandIcon from "./HandIcon";
+function HandButton({ value }) {
+  return (
+    <button>
+      <HandIcon value={value} />
+    </button>
+  );
+}
+export default HandButton;
