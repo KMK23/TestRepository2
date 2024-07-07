fc414df53c6edd9632fc3fc0fd2087d328c28b43
KMK23 <kmjnh34@gmail.com>
2024-07-07 21:59:58 +0900

    h

---
+++ react/03-moviepedia/src/h_practice/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import logoImg from "../assets/logo.png"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-nav">
+        <div className="App-nav-container">
+          <img className="App-logo" src
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- react/03-moviepedia/src/index.js
+++ react/03-moviepedia/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./practice/App";
 // import App from "./App";
+import App from "./h_practice/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
