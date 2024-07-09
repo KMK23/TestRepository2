0162ca241555da46ce1c334acc0eefcc05b95fac
KMK23 <kmjnh34@gmail.com>
2024-07-09 20:23:37 +0900

    Update App.js

--- react/03-moviepedia/src/h_practice/App.js
+++ react/03-moviepedia/src/h_practice/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import logoImg from "../assets/logo.png"
+import logoImg from "../assets/logo.png";
 
 function App() {
   return (
     <div className="App">
       <div className="App-nav">
         <div className="App-nav-container">
-          <img className="App-logo" src
+          <img className="App-logo" src={logoImg} />
+          <select>
+            <option>한국어</option>
+            <option>English</option>
+          </select>
         </div>
       </div>
     </div>
