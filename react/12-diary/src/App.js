a8b71afdb4f159b89c869230b1ed5bca55ffb779
KMK23 <kmjnh34@gmail.com>
2024-07-31 20:49:11 +0900

    1234

--- react/12-diary/src/App.js
+++ react/12-diary/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useReducer, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import NewPage from "./pages/NewPage";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<HomePage />} />
-            {/* <Route path="new" element={}/> */}
+            <Route path="new" element={<NewPage />} />
             {/* <Route path="edit" element={}/> */}
             {/* <Route path="diary" element={}/> */}
           </Route>
