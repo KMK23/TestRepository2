e12c77840728f999f432a394e0bee29ec275cb69
KMK23 <kmjnh34@gmail.com>
2024-08-13 21:14:54 +0900

    123

--- react/pracnote/src/App.js
+++ react/pracnote/src/App.js
@@ -4,29 +4,7 @@ import PracticeComponent from "./components/PracticeComponent";
 import { useRef, useState } from "react";
 
 function App() {
-  const input = useRef();
-  const [inputValue, setInputValue] = useState("");
-  const [click, setClick] = useState("");
-  const [validation, setValidation] = useState("");
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleButtonClick = () => {
-    setClick(true);
-    setValidation(inputValue === "0000");
-  };
-  return (
-    <div>
-      <input
-        ref={input}
-        onChange={handleChange}
-        className={click ? (validation ? "success" : "fail") : ""}
-      />
-      <Button onClick={handleButtonClick}>검증하기</Button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
